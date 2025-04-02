c0234a64c8849ee35d93b3fa16136d8a14bd964d
Chaitanya <chaitanya.j@brilliantinfosys.com>
2025-04-02 14:09:09 +0530

    commit all

--- Repository/S2SOrderViewRepo.cs
+++ Repository/S2SOrderViewRepo.cs
@@ -25,7 +25,7 @@ namespace S2SOMSAPI.Repository
             //DataSet ds1 = new DataSet();
             //string OmsorderNo = "";
             //OmsorderNo = req.OmsorderNo;
-            zyx
+            
             var ProductList = new List<Skulist>();
             try
             {
